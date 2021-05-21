3b0cdd4f93e13efaa01f6d985c27ced1cb90af66
Bogdan Peste <pestebogdan@yahoo.com>
2021-05-21 11:27:13 +0300

    removed dedicated serviceAccount for GKE

--- environments/dev/gke.tf
+++ environments/dev/gke.tf
@@ -1,7 +1,7 @@
-resource "google_service_account" "default" {
-  account_id   = "service-account-id"
-  display_name = "Service Account"
-}
+# resource "google_service_account" "default" {
+#   account_id   = "service-account-id"
+#   display_name = "Service Account"
+# }
 
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-cluster"
@@ -29,7 +29,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     machine_type = "e2-medium"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
+    # service_account = google_service_account.default.email
     oauth_scopes    = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
